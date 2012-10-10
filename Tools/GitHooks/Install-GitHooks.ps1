7017c08fdf9953270b22fc38321f0f6599817b41
Michael Naumov <michael.naumov@readify.net>
2012-10-10 18:40:53 +1100

    Add Install-GitHooks

---
+++ Tools/GitHooks/Install-GitHooks.ps1
@@ -0,0 +1,17 @@
+#requires -version 2.0
+
+[CmdletBinding()]
+param (
+)
+
+$scriptFolder = Split-Path $MyInvocation.MyCommand.Path -Parent
+$repoRoot = Join-Path $scriptFolder "..\..\"
+$gitHooksFolder = Join-Path $repoRoot ".git\hooks"
+
+if (-not (Test-Path $gitHooksFolder)) {
+    throw "Failed to locate .git\hooks directory"
+}
+
+Get-ChildItem -Path $scriptFolder | `
+    Where-Object -Filter { $_.Extension -eq "" } | `
+    Copy-Item -Destination $gitHooksFolder
