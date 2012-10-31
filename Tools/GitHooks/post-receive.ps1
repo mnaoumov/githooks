3febdfd782af78e6857f3427e6ba048af7bd79e8
Michael Naumov <michael.naumov@readify.net>
2012-10-31 17:54:32 +1100

    Add post-receive hook

---
+++ Tools/GitHooks/post-receive.ps1
@@ -0,0 +1,36 @@
+#requires -version 2.0
+
+[CmdletBinding()]
+param
+(
+    [string] $OldRef,
+    [string] $NewRef,
+    [string] $RefName
+)
+
+$script:ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+function PSScriptRoot { $MyInvocation.ScriptName | Split-Path }
+
+. "$(PSScriptRoot)\Common.ps1"
+
+Trap [Exception] `
+{
+    ProcessErrors $_
+}
+
+if ($RefName -notlike "refs/heads/*")
+{
+    Write-Debug "$RefName is not a branch commit"
+    ExitWithSuccess
+}
+
+$branchName = $RefName -replace "refs/heads/"
+
+$nextBranch = (Get-HooksConfiguration).Merges.Merge | `
+    Where-Object { ($_.branch -eq $branchName) } | `
+    Select-Object -ExpandProperty into -First 1
+
+Write-Warning "*****************************************************************************************************"
+Write-Warning "You pushed branch '$branchName'. Please merge it to the branch '$nextBranch' and push it as well ASAP"
+Write-Warning "*****************************************************************************************************"
