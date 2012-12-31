b546831988572d916db99ae5a720f7c3db1894c4
Michael Naumov <michael.naumov@readify.net>
2013-01-01 04:56:03 +1100

    Implemented ForcePush

--- ForcePush.ps1
+++ ForcePush.ps1
@@ -10,4 +10,55 @@ Set-StrictMode -Version Latest
 function PSScriptRoot { $MyInvocation.ScriptName | Split-Path }
 Trap { throw $_ }
 
-Write-Host "You are going to "
+
+. "$(PSScriptRoot)\Common.ps1"
+
+function Main
+{
+    $branchName = Get-CurrentBranchName
+
+    Write-Host "Please privide a reason why do you need to do force push of branch '$branchName' and bypass server-side hooks. Leave it blank if you want to cancel."
+    $reason = Read-Host -Prompt "Reason"
+
+    if (-not $reason)
+    {
+        Write-Warning "Reason was not provided. Force push cancelled"
+        exit 1
+    }
+
+    $userName = git config user.name
+
+    Enable-ForcePush -UserName $userName -Reason $reason
+
+    git push origin $branchName
+
+    Disable-ForcePush -UserName $userName
+}
+
+function Get-CurrentBranchName
+{
+    git rev-parse --abbrev-ref HEAD
+}
+
+function Enable-ForcePush
+{
+    param
+    (
+        [string] $UserName,
+        [string] $Reason
+    )
+
+    Write-Host "Force push is temporary enabled for '$UserName' because of the reason '$Reason'"
+}
+
+function Disable-ForcePush
+{
+    param
+    (
+        [string] $UserName
+    )
+
+    Write-Host "Force push is disabled for '$UserName'"
+}
+
+Main
