0f542bf5830fc91b0fa6bb9bec8243265555a15a
Michael Naumov <michael.naumov@readify.net>
2013-01-25 23:30:39 +1100

    post-receive add push date

--- Tools/GitHooks/post-receive.ps1
+++ Tools/GitHooks/post-receive.ps1
@@ -23,9 +23,32 @@ if ($RefName -notlike "refs/heads/*")
 
 $branchName = $RefName -replace "refs/heads/"
 
+if (-not (Test-KnownBranch $branchName))
+{
+    Write-Debug "$branchName is not known branch."
+    ExitWithSuccess
+}
+
 $nextBranch = Get-NextBranchName $branchName
 
 if ($nextBranch -ne $null)
 {
-    Write-HooksWarning "You pushed branch '$branchName'. Please merge it to the branch '$nextBranch' and push it as well ASAP.`nSee wiki-url/index.php?title=Git#Unmerged_changes"
+    $allowedMergeIntervalInHours = [Convert]::ToInt32((Get-HooksConfiguration).Pushes.allowedMergeIntervalInHours)
+    Write-HooksWarning "You pushed branch '$branchName'. Please merge your changes into '$nextBranch' and push it as well during next $allowedMergeIntervalInHours hours.`nSee wiki-url/index.php?title=Git#Unmerged_changes"
+}
+
+$reflog = git log -1 -g --date=iso --format=%gD $branchName
+if ($reflog -notmatch ".*@\{(?<date>.*)\}")
+{
+    Write-HooksWarning "Cannot parse reflog date: $reflog"
+    ExitWithSuccess
+}
+
+$pushDate = $Matches.date
+
+$commits = @(git rev-list "$OldRef..$NewRef" --first-parent)
+
+foreach ($commit in $commits)
+{
+    git notes --ref=push-date add -m $pushDate $commit
 }
